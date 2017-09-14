51dc9215eab4086f8d520cd5ecaf85a21c95963a
tucchun <tucchun@hotmail.com>
2017-09-14 20:41:50 +0800

    更新

--- src/inhabitant/articlelist/index.js
+++
@@ -1,5 +0,0 @@
-import Articles from '../../components/article/articles';
-import React from 'react';
-import ReactDom from 'react-dom';
-
-ReactDom.render(<div><Articles/></div>, document.getElementById('root'));
