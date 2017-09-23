11e63086b88d41a057c77a7b54585b2f752e9c8f
tucchun <tucchun@hotmail.com>
2017-09-23 19:58:26 +0800

    同步

--- src/page/articlelist/index.js
+++ src/page/articlelist/index.js
@@ -3,58 +3,19 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Util from '../../../assets/js/Util';
 
-const articleData = [
-  {
-    article_id: '12123',
-    article_category: 'sdfasdf',
-    article_title: 'asdf',
-    article_author: 'dfgdf',
-    create_dateStr: 'efdf',
-    article_img_url: 'sdfse'
-  }, {
-    article_id: '123',
-    article_category: 'sdf',
-    article_title: 'fdss',
-    article_author: 'essd',
-    create_dateStr: 'fsd',
-    article_img_url: 'hrr'
-  }, {
-    article_id: '421',
-    article_category: '12sds',
-    article_title: 'hdfg',
-    article_author: 'ertd',
-    create_dateStr: 'fghf',
-    article_img_url: 'wrwer'
-  }
-];
-
-const categoriesData = [
-  {
-    "lookup_code": '123123',
-    "lookup_value": "菜单一"
-  }, {
-    "lookup_code": '1212',
-    "lookup_value": "菜单二"
-  }
-];
 
 class Index extends React.Component {
   constructor(props) {
     super(props);
-    fetch("../../assets/rss/getarticlelist.json").then(response => {
-      return response.json();
-    }).then(data => {
-
-      console.log(data);
-    }).catch(err => {
-      console.log(err)
-    });
-    console.log("constructor");
+    this.state = {
+      articleData: [],
+      categoriesData: []
+    };
   }
 
   render() {
     console.log("render");
-    return (<Articles ArticlesData={articleData} CategoriesData={categoriesData}/>);
+    return (<Articles ArticlesData={this.state.articleData} CategoriesData={this.state.categoriesData}/>);
   }
 
   componentWillMount() {
@@ -62,6 +23,17 @@ class Index extends React.Component {
   }
 
   componentDidMount() {
+    this.fetchArticleData().then(data => {
+      this.setState({articleData: data});
+    }).catch(function(err) {
+      Util.alert(err);
+    });
+
+    this.fetchCategoriesData().then(data => {
+      this.setState({categoriesData: data});
+    }).catch(function(err) {
+      Util.alert(err);
+    });
     console.log("componentDidMount");
   }
 
@@ -71,6 +43,7 @@ class Index extends React.Component {
 
   shouldComponentUpdate() {
     console.log("shouldComponentUpdate");
+    return true;
   }
 
   componentWillUpdate() {
@@ -84,9 +57,32 @@ class Index extends React.Component {
   componentWillUnmount() {
     console.log("componentWillUnmount");
   }
+
+  fetchArticleData() {
+    return fetch("../../assets/rss/getarticlelist.json").then(response => {
+      return response.json();
+    }).then(data => {
+      if (data.ret_code === 1) {
+        return data.ret_data.articleList;
+      } else {
+        throw new Error(data.ret_msg || "查询信息错误！");
+      }
+    });
+  }
+
+  fetchCategoriesData() {
+    return fetch("../../assets/rss/getarticlecategory.json").then(response => {
+      return response.json();
+    }).then(data => {
+      if (data.ret_code === 1) {
+        return data.ret_data;
+      } else {
+        throw new Error(data.ret_msg || "查询信息错误！");
+      }
+    });
+  }
+
 }
-
-
 
 ReactDom.render(
   <Index/>, document.getElementById('root'));
